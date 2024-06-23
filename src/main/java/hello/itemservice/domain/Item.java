a09778130a3a0329f1f39c9ba158299c53ebc1b2
yunhye-choi <anffl98@gmail.com>
2024-06-23 17:23:10 +0900

    [feat] JPA 적용1 - 개발

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="item_name", length=10) // name은 생략 가능 (snake_case <-> camelCase 자동 지원)
     private String itemName;
     private Integer price;
     private Integer quantity;
